d1ac83ddf65a176dd4482876ffcc68f732631f9b
Antony Cabeza <antonycabeza@gmail.com>
2023-04-16 23:25:23 -0400

    MenuPage descomentado

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -1,40 +1,36 @@
-// import ProductCard from '@components/ProductCard';
-// import { TProduct } from '@/index';
+import ProductCard from '@components/ProductCard';
+import { TProduct } from '@/index';
 
-// const getProducts = async () => {
-//   // SSR
-//   // const response = await fetch('http://localhost:3000/api/products', {
-//   //   cache: 'no-store',
-//   // });
+const getProducts = async () => {
+  // SSR
+  // const response = await fetch('http://localhost:3000/api/products', {
+  //   cache: 'no-store',
+  // });
 
-//   // Incremental Static Regeneration (ISR)
-//   const response = await fetch('http://localhost:3000/api/products', {
-//     next: {
-//       revalidate: 60,
-//     },
-//   });
+  // Incremental Static Regeneration (ISR)
+  const response = await fetch('https://lilos-cake.vercel.app/api/products', {
+    next: {
+      revalidate: 60,
+    },
+  });
 
-//   const data = await response.json();
+  const data = await response.json();
 
-//   return data;
-// };
+  return data;
+};
 
-// export default async function MenuPage() {
-//   const productsData = getProducts();
-//   const [{ data: products }] = await Promise.all([productsData]);
+export default async function MenuPage() {
+  const productsData = getProducts();
+  const [{ data: products }] = await Promise.all([productsData]);
 
-//   return (
-//     <div className='flex flex-col items-center p-6 pt-3 gap-6 relative md:pt-14'>
-//       <h2 className='mb-2 font-lobsterTwo text-3xl'>Menú</h2>
-//       <section className='w-full flex flex-wrap justify-center gap-6'>
-//         {products.map((product: TProduct) => (
-//           <ProductCard key={product.id} product={product} />
-//         ))}
-//       </section>
-//     </div>
-//   );
-// }
-
-export default function MenuPage() {
-  return <div>menu</div>;
+  return (
+    <div className='flex flex-col items-center p-6 pt-3 gap-6 relative md:pt-14'>
+      <h2 className='mb-2 font-lobsterTwo text-3xl'>Menú</h2>
+      <section className='w-full flex flex-wrap justify-center gap-6'>
+        {products.map((product: TProduct) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </section>
+    </div>
+  );
 }
